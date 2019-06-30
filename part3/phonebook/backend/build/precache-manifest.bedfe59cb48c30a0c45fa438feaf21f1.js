d7c59c3095198051be74bd53b8bac253c048c715
Sean Keever <swkeever@gmail.com>
2019-06-30 10:16:32 -0700

    buildui

---
+++ part3/phonebook/backend/build/precache-manifest.bedfe59cb48c30a0c45fa438feaf21f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d082ec8def5a01a2cb3675709588529d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3a104210d0426ec5687",
+    "url": "/static/css/main.cfc154d4.chunk.css"
+  },
+  {
+    "revision": "6d1c51b22fbbd1e8977c",
+    "url": "/static/js/2.a16fff3d.chunk.js"
+  },
+  {
+    "revision": "d3a104210d0426ec5687",
+    "url": "/static/js/main.66155288.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

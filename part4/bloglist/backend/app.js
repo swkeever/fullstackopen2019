c1abca628a390d1e8663d210c92a53116a1f53b5
Sean Keever <swkeever@gmail.com>
2019-07-01 08:36:23 -0700

    adds part4 backend

---
+++ part4/bloglist/backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const blogsRouter = require('./controllers/blogs');
+const config = require('./utils/config');
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true });
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
